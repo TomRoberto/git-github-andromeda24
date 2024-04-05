06f9877d4230a2fa4c89330d87b9c966d7891b61
BastienLairis <bastien.lairis@gmail.com>
2024-04-05 11:38:46 +0200

    Add Footer Component on bastien-branch

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ background: "salmon", padding: "30px" }}>
+      <p>Je suis le footer par bastien-branche</p>
+    </footer>
+  );
+};
+
+export default Footer;
