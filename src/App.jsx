3449779650cb5ab893b715440bbd8d3ab73fd551
Tom ROBERTO <tom.roberto31@gmail.com>
2024-04-05 11:21:51 +0200

    added Header comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   console.log("hello");
   return (
     <>
+      <Header />
       <h1 style={{ color: "red" }}>Je suis le titre</h1>
       <h2 style={{ color: "green" }}>Coucou</h2>
       <h3>Bonjour</h3>
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: "purple" }}>Je suis le Header</header>
+  );
+};
+
+export default Header;
